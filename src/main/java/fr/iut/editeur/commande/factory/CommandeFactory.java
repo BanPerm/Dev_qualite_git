003a1167132a1c11219e2acb6c6f7fe882419894
Peter Poirrier <peter.poirrier@etu.umontpellier.fr>
2023-11-08 08:55:27 +0100

    Création remplacer

--- src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
+++ src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
@@ -18,6 +18,7 @@ public class CommandeFactory {
 
     public Commande createCommand(String name, Document document, String[] parameters) {
         switch (name) {
+            case "remplacer" : return new CommandeRemplacer(document, parameters);
             case "ajouter" : return new CommandeAjouter(document, parameters);
             default: return null;
         }
